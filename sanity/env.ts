52f84401516178fbdaede64d9f55e1cef7248025
Ryan Le <ryanle@live.com>
2024-05-14 15:40:26 -0400

    fix: env vars

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,13 +1,13 @@
 export const apiVersion =
-  process.env.SANITY_API_VERSION || '2023-11-03';
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-11-03';
 
 export const dataset = assertValue(
-  process.env.SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: SANITY_DATASET'
 );
 
 export const projectId = assertValue(
-  process.env.SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: SANITY_PROJECT_ID'
 );
 
